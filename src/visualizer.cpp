584d965cd240fa800f45db56f3b79054903b2e47
Marc Delorme <delormemarco@gmail.com>
2018-09-22 20:38:35 +0900

    Implement a simulator of job scheduler

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -4,6 +4,8 @@
 #include "imgui_internal.h"
 
 #include <random>
+#include <memory>
+#include <assert.h>
 
 namespace
 {
@@ -33,6 +35,7 @@ ImU32 g_Colors[] = {
 };
 
 int g_CoreNumber = 4;
+int g_FramePoolSize = 4;
 float g_TimeBoxAverageSize = 100.f;
 int g_TimeBoxNumber = 100;
 
@@ -89,13 +92,109 @@ void DrawTimeBox(ImVec2 origin, const TimeBox& timebox)
 
 }
 
+struct Job
+{
+    float duration;
+    int frame_index;
+};
+
+struct Core
+{
+    int index;
+    float time = 0.f;
+};
+
+class Simulator
+{
+public:
+    Simulator(int core, int frame_pool, float stddev)
+        : m_core_count(core)
+        , m_frame_pool_size(frame_pool)
+        , m_frame_count(0)
+        , m_generator(0)
+        , m_distribution(100.f * (1.f - stddev), 100.f *(1.f + stddev))
+    {
+        for (int i = 0; i < m_core_count; i++)
+        {
+            m_cores.emplace_back();
+        }
+        for (int i = 0; i < m_core_count; i++)
+        {
+            m_cores[i].index = i;
+        }
+
+        for (int i = 0; i < m_frame_pool_size; i++)
+        {
+            m_job_queue.push_back(generate());
+        }
+    }
+
+    void step()
+    {
+        auto* min_core = &m_cores[0];
+        for (auto& c : m_cores)
+        {
+            if (c.time < min_core->time)
+            {
+                min_core = &c;
+            }
+        }
+
+        Job j = pop_job();
+
+        m_timeboxes.emplace_back(min_core->index, j.frame_index, min_core->time, min_core->time + j.duration, "Job", g_Colors[j.frame_index % array_size(g_Colors)]);
+        m_max = ImMax(m_max, TimeBoxP1(m_timeboxes.back()));
+        min_core->time += j.duration;
+    }
+
+    const std::vector<TimeBox>& get_timeboxes() const { return m_timeboxes; }
+    const ImVec2& get_max() const { return m_max; }
+    const std::deque<Job>& get_queue() const { return m_job_queue; }
+
+private:
+    Job generate()
+    {
+        Job j;
+        j.duration = m_distribution(m_generator);
+        j.frame_index = m_frame_count;
+        m_frame_count += 1;
+        return j;
+    }
+
+    Job pop_job()
+    {
+        assert(!m_job_queue.empty());
+        Job j = m_job_queue.front();
+        m_job_queue.pop_front();
+        m_job_queue.push_back(generate());
+        return j;
+    }
+
+
+private:
+    int m_core_count;
+    int m_frame_pool_size;
+    int m_frame_count;
+    ImVec2 m_max;
+
+    std::mt19937 m_generator;
+    std::uniform_real_distribution<> m_distribution;
+
+    std::vector<Core> m_cores;
+    std::deque<Job> m_job_queue;
+    std::deque<int> m_frame_pool;
+    std::vector<TimeBox> m_timeboxes;
+};
+
+
 void DrawVisualizer()
 {
     bool yes = true;
     static float stddev = 0.5f;
     static bool generate = true;
-    static std::vector<TimeBox> timeboxes;
-    static ImVec2 max;
+    static bool step = false;
+    static bool autoStep = false;
+    static std::unique_ptr<Simulator> simulator;
 
     std::mt19937 gen(0);
     std::uniform_real_distribution<> dis(1.f - stddev, 1.f + stddev);
@@ -109,29 +208,19 @@ void DrawVisualizer()
 
     if (generate)
     {
-        max = ImGui::GetCursorPos();
-        timeboxes.clear();
-        timeboxes.reserve(g_TimeBoxNumber * g_CoreNumber);
-        for (int core = 0; core < g_CoreNumber; core++)
-        {
-            float time = 0.f;
-            for (int t = 0; t < g_TimeBoxNumber; t++)
-            {
-                float timespan = g_TimeBoxAverageSize * random();
-                auto timebox = TimeBox(core, t, time, time + timespan, "Frame", g_Colors[col]);
-                timeboxes.push_back(timebox);
-                time += timespan;
-                max = ImMax(max, TimeBoxP1(timebox));
-                col = (col + 1) % array_size(g_Colors);
-            }
-        }
+        simulator = std::make_unique<Simulator>(g_CoreNumber, g_FramePoolSize, stddev);
         generate = false;
     }
-    
+    if (step || autoStep)
+    {
+        simulator->step();
+        step = false;
+    }
+
     float windowMin = ImGui::GetScrollX();
     float windowMax = windowMin + ImGui::GetWindowSize().x;
     int count = 0;
-    for (const auto& t : timeboxes)
+    for (const auto& t : simulator->get_timeboxes())
     {
         if (t.start_time <= windowMax && t.end_time >= windowMin)
         {
@@ -141,13 +230,14 @@ void DrawVisualizer()
     }
 
     // Add an offset to scroll a bit more than the max of the timeline
-    ImGui::SetCursorPos(max + ImVec2(100.f, 0.f));
+    ImGui::SetCursorPos(simulator->get_max() + ImVec2(100.f, 0.f));
 
     ImGui::End();
 
 
     ImGui::Begin("Frame Centric Simulation - Options", &yes);
     ImGui::SliderInt("Core Number", &g_CoreNumber, 1, 16);
+    ImGui::SliderInt("Frame Pool Size", &g_FramePoolSize, 1, 16);
     ImGui::SliderFloat("TimeBox Size", &g_TimeBoxAverageSize, 10.f, 200.f);
     ImGui::SliderInt("TimeBox Number", &g_TimeBoxNumber, 0, 10000);
     ImGui::Text("Rendered Count %d", count);
@@ -158,6 +248,16 @@ void DrawVisualizer()
     if (ImGui::Button("Simulate"))
     {
         generate = true;
+    }
+    if (ImGui::Button("Step"))
+    {
+        step = true;
+    }
+    ImGui::Checkbox("Auto Step", &autoStep);
+
+    for (auto& j : simulator->get_queue())
+    {
+        ImGui::BulletText("Job: %d, duration = %f", j.frame_index, j.duration);
     }
     ImGui::End();
 }
--- src/visualizer.h
+++ src/visualizer.h
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <sstream>
+#include <deque>
+#include <vector>
 
 struct TimeBox
 {
