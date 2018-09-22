56576453b364155d2b515e617b3dd4d18fc72f5e
Marc Delorme <delormemarco@gmail.com>
2018-09-22 17:02:48 +0900

    Add text to timebox visualization

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -18,6 +18,8 @@ ImU32 g_Yellow = ImGui::ColorConvertFloat4ToU32(ImVec4(1.0f, 1.0f, 0.0f, 1.0f));
 ImU32 g_Cyan = ImGui::ColorConvertFloat4ToU32(ImVec4(0.0f, 1.0f, 1.0f, 1.0f));
 ImU32 g_Magenta = ImGui::ColorConvertFloat4ToU32(ImVec4(1.0f, 0.0f, 1.0f, 1.0f));
 ImU32 g_Grey = ImGui::ColorConvertFloat4ToU32(ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
+ImU32 g_LightGrey = ImGui::ColorConvertFloat4ToU32(ImVec4(0.7f, 0.7f, 0.7f, 1.0f));
+ImU32 g_Black = ImGui::ColorConvertFloat4ToU32(ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
 
 ImU32 g_Colors[] = {
     g_White,
@@ -31,7 +33,7 @@ ImU32 g_Colors[] = {
 };
 
 int g_CoreNumber = 4;
-int g_TimeBoxNumber = 700;
+int g_TimeBoxNumber = 100;
 
 template<class T, size_t N>
 constexpr size_t array_size(T(&)[N]) { return N; }
@@ -52,6 +54,19 @@ void DrawTimeBox(ImVec2 origin, const TimeBox& timebox)
     auto p0 = win + origin + TimeBoxP0(timebox);
     auto p1 = win + origin + TimeBoxP1(timebox);
     drawList->AddRectFilled(p0, p1, timebox.color, 3.5f, ImDrawCornerFlags_All);
+
+    ImVec2 size = p1 - p0;
+    ImU32 c = 0xff000000 | ~timebox.color;
+    ImVec2 textFrameSize = ImGui::CalcTextSize(timebox.frame_index.c_str());
+    ImVec2 offsetFrame = (size - textFrameSize) * 0.5f;
+    offsetFrame.x = 2.f;
+    drawList->AddText(p0 + offsetFrame, c, timebox.frame_index.c_str());
+
+    ImVec2 textNameSize = ImGui::CalcTextSize(timebox.name.c_str());
+    ImVec2 offsetName = (size - textFrameSize) * 0.5f;
+    offsetName.x = size.x - textNameSize.x - 2.f;
+    auto clip = ImVec4(p0.x + textFrameSize.x + 2, p0.y, p1.x, p1.y);
+    drawList->AddText(nullptr, 0.f, p0 + offsetName, c, timebox.name.c_str(), nullptr, 0.f, &clip);
 }
 
 }
@@ -72,8 +87,8 @@ void DrawVisualizer()
         float time = 0.f;
         for (int t = 0; t < g_TimeBoxNumber; t++)
         {
-            float timespan = 30.f * random();
-            auto timebox = TimeBox(core, time, time + timespan, g_Colors[col]);
+            float timespan = 70.f * random();
+            auto timebox = TimeBox(core, t, time, time + timespan, "Very Very Long Hello World", g_Colors[col]);
             DrawTimeBox(origin, timebox);
             time += timespan;
             max = ImMax(max, TimeBoxP1(timebox));
--- src/visualizer.h
+++ src/visualizer.h
@@ -1,17 +1,27 @@
 #include <stdint.h>
 
+#include <string>
+#include <sstream>
+
 struct TimeBox
 {
-    TimeBox(int index, float start, float end, uint32_t c = 0xffffffff)
+    TimeBox(int index, int frame, float start, float end, const std::string& n, uint32_t c = 0xffffffff)
         : core_index(index)
         , start_time(start)
         , end_time(end)
+        , name(n)
         , color(c)
-    {}
+    {
+        std::stringstream s;
+        s << frame << "-";
+        frame_index = s.str();
+    }
 
     int core_index;
+    std::string frame_index;
     float start_time;
     float end_time;
+    std::string name;
     uint32_t color;
 };
 
